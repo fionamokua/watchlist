9188551482708d4e83e74b022db9af87873f2f07
fionamokua <mokuafiona@gmail.com>
2024-02-29 12:14:54 +0300

    onetomany modelrelationship

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import StreamingPlartformAV,WatchDetailAV,WatchListAV,StreamingPlartformDetailAV
 urlpatterns = [
     path('list/',WatchListAV.as_view(),name="movie_list"),
-    path("<int:pk>/",WatchDetailAV.as_view(),name="movie_details"),
+    path("list/<int:pk>/",WatchDetailAV.as_view(),name="movie_details"),
     path("stream/",StreamingPlartformAV.as_view(),name="stream"),
     path("stream/<int:pk>/",StreamingPlartformDetailAV.as_view(),name="streamdetail"),
 ]
